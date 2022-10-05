155b8cb5c168bfa6e4ecc5ac26d316460e796b22
Dongwoo Lee <eastrain2910@gmail.com>
2022-10-05 17:34:59 +0900

    1005 database

---
+++ Database/02_db/articles/forms.py
@@ -0,0 +1,42 @@
+from django import forms
+from .models import Article, Comment
+
+
+class ArticleForm(forms.ModelForm):
+    title = forms.CharField(
+        label='제목',
+        widget=forms.TextInput(
+            attrs={
+                'class': 'my-title',
+                'placeholder': 'Enter the title',
+                'maxlength': 10,
+            }
+        )
+    )
+
+    content = forms.CharField(
+        label='내용',
+        widget=forms.Textarea(
+            attrs={
+                'class': 'my-content',
+                'placeholder': 'Enter the content',
+                'rows': 5,
+                'cols': 50,
+            }
+        ),
+        error_messages={
+            'required': '내용을 입력하세요.',
+        }
+    )
+
+    class Meta:
+        model = Article
+        # fields = '__all__'
+        exclude = ('user',)
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        # fields = '__all__'
+        exclude = ('article', 'user',)
