d59aa2b9e65071f7c5d6b56e93bf3d0ab388981f
Dongwoo Lee <eastrain2910@gmail.com>
2022-11-03 18:21:24 +0900

    1103 vue

---
+++ Vue/03_vue/vue-cli-todo-practice/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <TodoList :todos="todos" @delete-todo="deleteTodo"/>
+    <TodoForm @create-todo="createTodo"/>
+  </div>
+</template>
+
+<script>
+import TodoForm from '@/components/TodoForm'
+import TodoList from '@/components/TodoList'
+
+
+export default {
+  name: 'App',
+  components: {
+    TodoForm,
+    TodoList,
+  }, 
+  data: function () {
+    return {
+      todos: [],
+    }
+  },
+  methods: {
+    createTodo: function (todoTitle) {
+      const todo = {
+        title: todoTitle
+      }
+      this.todos.push(todo)
+    },
+    deleteTodo: function (todo) {
+      const index = this.todos.indexOf(todo)
+      this.todos.splice(index, 1)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
